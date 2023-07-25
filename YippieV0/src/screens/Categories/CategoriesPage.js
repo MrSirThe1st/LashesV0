4c2648d41f5e4701f4f97fd5f97b0e396c8a1630
MrSirThe1st <marcilungambuyu@gmail.com>
2023-07-25 21:59:26 +0200

    categories modifications

--- YippieV0/src/screens/Categories/CategoriesPage.js
+++ YippieV0/src/screens/Categories/CategoriesPage.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View, SafeAreaView, FlatList, Image, Pressable } from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  Image,
+  Pressable,
+} from "react-native";
+import React from "react";
 
 const services = [
   {
     id: "1",
-    image: require("../../assets/homeAssets/buy.png"),
-    name: "event",
+    image: require("../../assets/homeAssets/image1.jpg"),
+    name: "name",
   },
   {
     id: "2",
@@ -34,100 +42,99 @@ const services = [
   },
   {
     id: "7",
-    image: require('../../assets/homeAssets/nail-art.png'),
+    image: require("../../assets/homeAssets/nail-art.png"),
     name: "name",
   },
   {
     id: "8",
-    image: require('../../assets/homeAssets/nail-art.png'),
+    image: require("../../assets/homeAssets/nail-art.png"),
     name: "name",
   },
   {
     id: "9",
-    image: require('../../assets/homeAssets/nail-art.png'),
+    image: require("../../assets/homeAssets/nail-art.png"),
     name: "name",
   },
   {
     id: "10",
-    image: require('../../assets/homeAssets/nail-art.png'),
+    image: require("../../assets/homeAssets/nail-art.png"),
     name: "name",
   },
   {
     id: "11",
-    image: require('../../assets/homeAssets/nail-art.png'),
+    image: require("../../assets/homeAssets/nail-art.png"),
     name: "name",
   },
   {
     id: "12",
-    image: require('../../assets/homeAssets/nail-art.png'),
+    image: require("../../assets/homeAssets/nail-art.png"),
     name: "name",
   },
-  
 ];
 
-const Item = ({image, name}) => (
+const Item = ({ image, name }) => (
   <View style={styles.itemContainer}>
-      <Pressable style={styles.pressable}>
-        <View style={styles.pressableImage}>
-          <Image source={image} style={styles.image} />
-          <View>
-            <Text style={styles.text}>{name}</Text>
-          </View>
+    <Pressable style={styles.pressable}>
+      <View style={styles.pressableImage}>
+        <Image source={image} style={styles.image} resizeMode="cover"/>
+      </View>
+      <View>
+          <Text style={styles.text}>{name}</Text>
         </View>
-      </Pressable>
-    </View>
-)
+    </Pressable>
+  </View>
+);
 
 const CategoriesPage = () => {
-  const renderItem = ({ item }) => <Item image={item.image} name={item.name}/>
+  const renderItem = ({ item }) => <Item image={item.image} name={item.name} />;
 
   return (
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={services}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          numColumns={3} 
-        />    
-      </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={services}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id} 
+        numColumns={2}
+      />
+    </SafeAreaView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    flexDirection: 'row', 
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    backgroundColor:'white'
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    backgroundColor: "white",
   },
   itemContainer: {
-    width: '33%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal:8,
-    paddingVertical:20,
-    backgroundColor:'white'
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 8,
+    paddingVertical: 20,
+    backgroundColor: "white",
   },
   pressable: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   pressableImage: {
-    backgroundColor: '#f2f2f2',
-    paddingHorizontal:20,
-    paddingVertical:12,
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f2f2f2",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
-    width: 70,
-    height: 70,
+    width: 160,
+    height: 160,
+    borderRadius: 10,
   },
-  text:{
-    fontWeight:'bold'
-  }
+  text: {
+    fontWeight: "bold",
+    fontSize:20
+  },
 });
 
 export default CategoriesPage;
